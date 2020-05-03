aa02ff30d4fdff0dd76bb9da6a4108b9dfe573db
soshika <soshika1010@gmail.com>
2020-05-03 21:43:35 +0430

    working goooood

--- main.py
+++ main.py
@@ -3,9 +3,10 @@
 from time import sleep
 from datetime import datetime
 from selenium import webdriver
+import telegram
 
 
-def isExpired(now):
+def is_expired(now):
     if now.hour > 12:
         return True
     if now.hour == 12 and now.minute > 30:
@@ -20,31 +21,52 @@ def main():
 
     while True:
         now = datetime.now()
-        if isExpired(now):
-            print('robot is shutdown at: ', now)
-            break
+        # if is_expired(now):
+        #     print('robot is shutdown at: ', now)
+        #     break
 
-    urls = [
-        'https://www.sahamyab.com/hashtag/غگرجی/post',
-        'https://www.sahamyab.com/hashtag/بورس',
-        'https://www.sahamyab.com/hashtag/کالا',
-        'https://www.sahamyab.com/hashtag/درهآور',
-        'https://www.sahamyab.com/hashtag/تکشا',
-        'https://www.sahamyab.com/hashtag/داوه',
-        'https://www.sahamyab.com/hashtag/غگیلا',
-        'https://www.sahamyab.com/hashtag/سفاسی'
+    stocks = [
+
     ]
 
-    for url in urls:
-        driver = webdriver.Chrome(
-            executable_path='/Users/soshika/Documents/dev/python3/stocks/sell-signal/chromedriver')
-        driver.get(url)
+    contacts = [
 
-        title = driver.find_element_by_xpath(
-            '/html/body/div[1]/app-root/div/main/div/div/app-symbol-detail/div[2]/div[2]/div/table/td[1]/div/h1').text
-        print(title)
+    ]
+
+    for stock in stocks:
+        driver = webdriver.Chrome(executable_path='')
+        driver.get(stock[0])
+
+        print('robot is taking nap for 10 seconds!')
+        sleep(10)
+
+        buy_amount = int(str(driver.find_element_by_xpath('//*[@id="leftColumn"]/div[1]/div/app-queue-and-legal-real/div/div[1]/app-queue-table/div/div[1]/table/tbody/tr[2]/td[2]/span').text).replace(',', ''))
+        buy_count = int(str(driver.find_element_by_xpath('//*[@id="leftColumn"]/div[1]/div/app-queue-and-legal-real/div/div[1]/app-queue-table/div/div[1]/table/tbody/tr[2]/td[1]/span').text).replace(',', ''))
+
+        sell_amount = int(str(driver.find_element_by_xpath('//*[@id="leftColumn"]/div[1]/div/app-queue-and-legal-real/div/div[1]/app-queue-table/div/div[2]/table/tbody/tr[2]/td[2]/span').text).replace(',', ''))
+        sell_count = int(str(driver.find_element_by_xpath('//*[@id="leftColumn"]/div[1]/div/app-queue-and-legal-real/div/div[1]/app-queue-table/div/div[2]/table/tbody/tr[2]/td[3]/span').text).replace(',', ''))
 
         driver.close()
+
+        sell = sell_amount * sell_count
+        buy = buy_count * buy_amount
+
+        print('sell is: ', sell)
+        print('buy is: ', buy)
+
+        if sell >= buy:
+            ####################
+            # Use Telegram API #
+            ####################
+
+            for contact in contacts:
+                message = '{}, time comes to sell {} right now!'.format(contact[1], stock[1])
+                telegram.send_message(contact[0], message)
+
+        else:
+            for contact in contacts:
+                message = "Don't rush {}, just save the {}".format(contact[1], stock[1])
+                telegram.send_message(contact[0], message)
 
 
 if __name__ == '__main__':
